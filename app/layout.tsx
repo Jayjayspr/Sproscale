9e766349ebf08991b736af7b4a469dcee27aa2ed
Jay <jay.sprock00@gmail.com>
2026-03-29 13:16:02 +0200

    image cropped and vercel speed insights implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
 import './globals.css';
 import ClientWrapper from '../components/ClientWrapper';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ClientWrapper>
           {children}
         </ClientWrapper>
+        <SpeedInsights />
       </body>
     </html>
   );
